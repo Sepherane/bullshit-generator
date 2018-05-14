4c2397bd1c891feab0c8a6509dc3672ac410cc15
Bob <bob.vankamp@gmail.com>
2018-05-14 20:49:58 +0200

    basic chat bot for study questions

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,34 @@
 import sys
+import re
+
+data = [
+    {'match':["(.*)hoe lang( duurt)? de opleiding"], "message":"De opleiding duurt 4 jaar, maar soms loopt het iets uit."},
+    {'match':["wat leer (ik|je)( zoal)? tijdens de opleiding"], "message":"Je leert programmeren en verschillende skills die van pas kunnen komen in het bedrijfsleven."},
+    {'match':[], "message":""},
+    {'match':[], "message":""}
+]
 
 def waitForInput():
     message = input("")
     processMessage(message)
 
 def processMessage(message):
-    if(message == "bye"):
-        print("Bye :(")
+    if(re.match("^(doei|tot ziens)$", message)):
+        print("Tot ziens!")
         sys.exit(1)
-    print(">",message)
+    print(">", getMatches(message))
     waitForInput()
+
+def getMatches(message):
+    for option in data:
+        for match in option['match']:
+            if(re.match(match, message, re.IGNORECASE)):
+                return option['message']
+    return "Sorry, ik begreep je niet. Zou je het op een andere manier kunnen vragen?"
 
 
 def main():
+    print("Wat zou je willen weten?")
     waitForInput()
 
 main()
